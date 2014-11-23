9d460eec8f3c152923e49f4a2ee1c6e659612aea
Edan Kfir <edankfir82@gmail.com>
2014-11-23 14:36:58 -0800

    adding players resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 ScoreKeeper::Application.routes.draw do
+  
+  resources :players
+
   get "dashboard/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
